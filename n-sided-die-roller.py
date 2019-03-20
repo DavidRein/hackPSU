2cf4dc83014e9338489079456b2877a3ce39a6a4
David Reinoso <davidr1324@gmail.com>
2019-03-20 19:53:19 -0400

    modified for gui implementation

--- n-sided-die-roller.py
+++ n-sided-die-roller.py
@@ -1,29 +1,17 @@
 from random import randint
+import tkinter
+import tkinter.messagebox as tkMessageBox
 
 print("##n-Sided Dice Roller##")
-print("The instructions are simple:")
-print("\tinput a number greater than 1 (so 2 or more),")
-print("\tthe program outputs a random number from 1 to")
-print("\tthat number")
-print("To quit the program, type 'exit'")
-print("....................................")
-print("....................................")
-print("....................................")
 
-exitLoop = False
-while not exitLoop:
-    x = input("Please insert a maximum number:")
-    if(x == "exit"):
-        break
-    try:
-        x = int(x)
-    except ValueError:
-        print("Invalid input, please enter a positive integer >1")
-        continue
-    if(x < 2):
-        print("Invalid input, please enter an integer >1")
-        continue
-    print("...")
-    print(randint(1,x))
+def executeRoll():
+    tkMessageBox.showinfo("Result", randint(1,20))
+
+root = tkinter.Tk()
+
+rollButton = tkinter.Button(root, text = "Roll", command = lambda: executeRoll())
+
+rollButton.pack()
+root.mainloop()
 
 print("Exiting program, have a nice day! :)")
