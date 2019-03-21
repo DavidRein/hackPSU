c261f0e1a3b37c43d2c71263439b42dc98c83443
David Reinoso <davidr1324@gmail.com>
2019-03-21 14:38:23 -0400

    modified gui as class structure

--- n-sided-die-roller.py
+++ n-sided-die-roller.py
@@ -3,15 +3,25 @@ import tkinter
 import tkinter.messagebox as tkMessageBox
 
 print("##n-Sided Dice Roller##")
+class Application(tkinter.Frame):
 
-def executeRoll():
-    tkMessageBox.showinfo("Result", randint(1,20))
+    def __init__(self, root = None):
+        super().__init__(root)
+        self.root = root
+        self.pack()
+        self.setWidgets()
 
-root = tkinter.Tk()
+    def setWidgets(self):
+        self.rollButton = tkinter.Button(self.root,
+                                    text = "Roll",
+                                    command = lambda: self.executeRoll())
+        self.rollButton.pack()
 
-rollButton = tkinter.Button(root, text = "Roll", command = lambda: executeRoll())
+    def executeRoll(self):
+        tkMessageBox.showinfo("Result", randint(1,20))
 
-rollButton.pack()
-root.mainloop()
+tk = tkinter.Tk()
+app = Application(root = tk)
+app.mainloop()
 
 print("Exiting program, have a nice day! :)")
