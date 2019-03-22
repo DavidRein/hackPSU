663c06dff4b0fbe80e728c591fcefa90bdeba04a
David Reinoso <davidr1324@gmail.com>
2019-03-22 19:06:35 -0400

    image and text label added

--- n-sided-die-roller.py
+++ n-sided-die-roller.py
@@ -1,12 +1,14 @@
 from random import randint
 import tkinter
 import tkinter.messagebox as tkMessageBox
+from PIL import ImageTk , Image
 
 print("##n-Sided Dice Roller##")
 class Application(tkinter.Frame):
 
     def __init__(self, root = None):
         super().__init__(root)
+        self.result = 0
         self.root = root
         self.pack()
         self.setWidgets()
@@ -17,8 +19,22 @@ class Application(tkinter.Frame):
                                     command = lambda: self.executeRoll())
         self.rollButton.pack()
 
+        self.dieImage = Image.open("d20.png")
+        self.dieGraphic = ImageTk.PhotoImage(self.dieImage)
+        self.dieLabel = tkinter.Label(self.root,
+                                      image = self.dieGraphic)
+        self.dieLabel.image = self.dieGraphic
+        self.dieLabel.pack()
+
+        self.resultLabel = tkinter.Label(self.root,
+                                         text = str(self.result),
+                                         anchor = "center")
+        self.resultLabel.pack()
+
     def executeRoll(self):
-        tkMessageBox.showinfo("Result", randint(1,20))
+        self.result = randint(1,20)
+        self.resultLabel.config(text = self.result)
+        print("updating roll..." , self.result)
 
 tk = tkinter.Tk()
 app = Application(root = tk)
